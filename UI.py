38ba3010fe8d4241a0e2a1d9c3c61873744d4680
liz-areizaga <larei002@ucr.edu>
2023-03-02 16:28:34 -0800

    Write comment to log file
    Added function we can use to write to log file. Every input into the log file is preceded by the time stamp and followed by a newline.
    
    Used that function to record comments into log file.

--- UI.py
+++ UI.py
@@ -1,15 +1,24 @@
 import time
 import PySimpleGUI as sg
 
+logfile = "Logfile_2022.txt" #temp var-- change to ask file name when program starts
+
+def writeToLog (fileName, comment):
+    file = open(fileName, 'a')
+    file.write(time.strftime('%H:%M:%S') + ' ' )
+    file.write(comment)
+    file.write('\n')
+    file.close()
+
 def main_page(name):
     sg.theme('LightGray1')  #Can change theme https://www.geeksforgeeks.org/themes-in-pysimplegui/
-    font = ('Arial', 30)   
-      
+    font = ('Arial', 30)
+
     fileName = 'CanardII.txt' #Change later to read in manifest name
     names = name #Name of the employee logging in, passed in as argument of function
-    
+
     layout = [
-        [sg.Text(fileName,font = font,),sg.Text('',font = font,pad = (200,0),key = 'time'),sg.Text(names,font = font,pad = ((20,0),(0,0)))],      
+        [sg.Text(fileName,font = font,),sg.Text('',font = font,pad = (200,0),key = 'time'),sg.Text(names,font = font,pad = ((20,0),(0,0)))],
         #File name on top left, Time in the center, and employee name at the right
         #Employee Name is blank before anyone logs in
         [sg.Text('',size = (0,15))],
@@ -22,7 +31,7 @@ def main_page(name):
     ]
 
     window = sg.Window('Main Page', layout, size=(900, 700),finalize = True)
-    
+
     while True:
         event, values = window.read(timeout=10) #Timeout is required for updating clock
         if event == 'Login': #Employee clicks Login button
@@ -30,53 +39,55 @@ def main_page(name):
             signin_page(name,fileName,'main')
         elif event == 'Comments': #Employee clicks Comments button
             window.close()
-            comments_page(name, fileName, 'main') 
+            comments_page(name, fileName, 'main')
         elif event == 'Loading/Unloading': #Employee clicks Loading/Unloading button
             window.close()
-            unloading_loading_page(name,fileName) 
+            unloading_loading_page(name,fileName)
         elif event == 'Balancing': #Employee clicks Balancing button
             window.close()
             balancing_page(name,fileName)
         elif event == sg.WIN_CLOSED: #Employee clicks the X on the program
             exit()
         window['time'].update(time.strftime('%H:%M:%S')) #Update clock in real time (Military time, local time)
-                
+
     window.close()
 
 def signin_page(name, fileName, function_call): #function_call tells which function is calling this function
     sg.theme('LightGray1')  #Can change theme https://www.geeksforgeeks.org/themes-in-pysimplegui/
-    font = ('Arial',15)   
-    
+    font = ('Arial',15)
+
     layout = [
     [sg.Text('Sign in',font = font)],
     [sg.Text('Name', font = font), sg.InputText(size=(50,10),expand_y = True)], #Text box for signing in
     [sg.Submit(), sg.Cancel()] #Submit button and hitting enter both submits the text
     ]
-  
+
     window = sg.Window('Sign In Page', layout)
     event, values = window.read()
-    window.close()   
+    window.close()
     if function_call == 'main':
         main_page(values[0]) #Open the main page after closing the sign in page, passing in the name typed in as argument
     elif function_call == 'load unload':
         unloading_loading_page(values[0],fileName)
     elif function_call == 'balance':
         balancing_page(values[0],fileName)
-        
-        
+
+
 def comments_page(name, fileName,function_call): #function_call tells which function is calling this function
     sg.theme('LightGray1')  #Can change theme https://www.geeksforgeeks.org/themes-in-pysimplegui/
-    font = ('Arial',15)  
-    
+    font = ('Arial',15)
+
     layout = [
     [sg.Text('Enter your comment:')],
     [sg.Multiline(size=(50, 20))], #Multi-line input box, employee has to click 'Submit' to submit the comment, enter does not submit
     [sg.Button('Submit')]
     ]
-    
+
     window = sg.Window('Comment Page', layout, font = font)
     event, values = window.read()
-    window.close() 
+    window.close()
+    comment = values[0]
+    writeToLog(logfile, comment)
     if function_call == 'main':
         main_page(name)   #Open the main page after closing the comments page, passing in the name typed in as argument
     elif function_call == 'load unload':
@@ -84,19 +95,19 @@ def comments_page(name, fileName,function_call): #function_call tells which func
     elif function_call == 'balance':
         balancing_page(name,fileName)
     #TODO: Add variable to store the comments made
-    
+
 def unloading_loading_page(names,fileName):
     sg.theme('LightGray1')
     font = ('Arial',30)
-    
+
     layout = [
-    [sg.Text(fileName,font = font),sg.Text('',font = font,pad = (200,0),key = 'time'),sg.Text(names,font = font,pad = ((20,0),(0,0)))],      
+    [sg.Text(fileName,font = font),sg.Text('',font = font,pad = (200,0),key = 'time'),sg.Text(names,font = font,pad = ((20,0),(0,0)))],
     [sg.Text('',size = (0,3))],
     [[sg.B('',size=(3,1.125),pad = (0.1,0), key=(i,j)) for i in range (8)] for j in range(12)],
     [sg.Text('',size = (0,3))],
     [sg.Button('Comments',size=(10,2),font = ('Arial',14)), sg.Button('Done',pad = (200,0),size = (10,2),font = ('Arial',14)),sg.Button('Login',size=(10,2),font = ('Arial',14))]
     ]
-    
+
     window = sg.Window('Loading Unloading Page', layout, size=(900, 700),finalize = True)
     while True:
         event, values = window.read(timeout = 10)
@@ -112,19 +123,19 @@ def unloading_loading_page(names,fileName):
 
         #TODO add implementation for done button
     window.close()
-    
+
 def balancing_page(names,fileName):
     sg.theme('LightGray1')
     font = ('Arial',30)
-    
+
     layout = [
-    [sg.Text(fileName,font = font),sg.Text('',font = font,pad = (200,0),key = 'time'),sg.Text(names,font = font,pad = ((20,0),(0,0)))],      
+    [sg.Text(fileName,font = font),sg.Text('',font = font,pad = (200,0),key = 'time'),sg.Text(names,font = font,pad = ((20,0),(0,0)))],
     [sg.Text('',size = (0,3))],
     [[sg.B('',size=(3,1.125),pad = (0.1,0), key=(i,j)) for i in range (8)] for j in range(12)],
     [sg.Text('',size = (0,3))],
     [sg.Button('Comments',size=(10,2),font = ('Arial',14)), sg.Button('Done',pad = (200,0),size = (10,2),font = ('Arial',14)),sg.Button('Login',size=(10,2),font = ('Arial',14))]
     ]
-    
+
     window = sg.Window('Balancing Page', layout, size=(900, 700),finalize = True)
     while True:
         event, values = window.read(timeout = 10)
@@ -140,15 +151,12 @@ def balancing_page(names,fileName):
 
         #TODO add implementation for done button
     window.close()
-    
-    
-        
-    
+
+
+
+
 def main():
     main_page('') #First open main page with no employee name
-        
+
 if __name__ == '__main__':
     main()
-    
-    
-    
